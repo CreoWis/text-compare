50322248d5c4bc2cac573893658287d6d640c140
Joy Poddar <joypoddar.sd@gmail.com>
2026-03-23 12:26:41 +0530

    feat: update packages (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "./lib/index.ts"),
-      name: "react-beautiful-timeline",
-      fileName: (format) => `index.${format}.js`,
+      entry: resolve(__dirname, './lib/index.ts'),
+      name: 'react-beautiful-timeline',
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ["react", "react-dom", "tailwindcss"],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'tailwindcss'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tailwindcss: "tailwindcss",
-        },
-      },
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'ReactJSXRuntime',
+          tailwindcss: 'tailwindcss'
+        }
+      }
     },
     sourcemap: true,
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   plugins: [react(), dts({ rollupTypes: true })],
   css: {
     postcss: {
-      plugins: [tailwindcss],
-    },
-  },
+      plugins: [tailwindcss]
+    }
+  }
 });
